c4f7f063d1eaac1f0be0abb0159dd1d68d10fbb0
Joshiro19j <joshiroticomq@gmail.com>
2025-07-16 08:30:34 -0500

    Update Enemigo.java

--- Enemigo.java
+++ Enemigo.java
@@ -15,29 +15,24 @@ public class Enemigo {
         this.y = y;
     }
 
-    // Atacar al jugador
     public void atacar(Jugador jugador) {
         jugador.recibirDaño(this.ataque);
         System.out.println(tipo + " enemigo ataca al jugador. Salud del jugador: " + jugador.getSalud());
     }
 
-    // Recibir daño
     public void recibirDaño(int cantidad) {
         this.salud -= cantidad;
         if (this.salud < 0) this.salud = 0;
     }
-
-    // Verificar si el enemigo sigue vivo
+    
     public boolean estaVivo() {
         return this.salud > 0;
     }
 
-    // Mover el enemigo hacia abajo
     public void moverAbajo() {
         this.y++;
     }
 
-    // Getters
     public int obtenerX() {
         return x;
     }
@@ -46,7 +41,6 @@ public class Enemigo {
         return y;
     }
 
-    // Mostrar el estado del enemigo
     public void mostrarEstado() {
         System.out.println("Enemigo: " + tipo + " - Salud: " + salud);
     }
