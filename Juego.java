cce9e890d1377edb03f4814b581903aaa3eb46ef
Joshiro19j <joshiroticomq@gmail.com>
2025-07-17 22:23:22 -0500

    Juego.java

--- Juego.java
+++ Juego.java
@@ -1,135 +1,82 @@
 package battlespace;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Juego {
-    private final int ANCHO = 30;
-    private final int ALTO = 20;
-    private char[][] tablero;
-
     private Jugador jugador;
-    private List<Enemigo> enemigos;
-
-    private boolean juegoTerminado = false;
-    private int turno = 1;
-
-    private ManejadorDeEntrada manejadorDeEntrada;
+    private Enemigo[] enemigos;
+    private boolean juegoTerminado;
+    private int turno;
+    private Combate combate;
 
     public Juego(int cantidadEnemigos) {
-        tablero = new char[ALTO][ANCHO];
-        jugador = new Jugador(ANCHO / 2, ALTO - 2);
-        enemigos = new ArrayList<>();
-
+        this.jugador = new Jugador("Jugador", 9, 7);
+        this.enemigos = new Enemigo[cantidadEnemigos];
+        this.juegoTerminado = false;
+        this.turno = 1;
         generarEnemigos(cantidadEnemigos);
-
-        manejadorDeEntrada = new ManejadorDeEntrada();
-        manejadorDeEntrada.establecerJuego(this);
-        new Thread(manejadorDeEntrada).start();
+        this.combate = new Combate(jugador, enemigos);
     }
 
-    
     public void iniciarJuego() {
         juegoTerminado = false;
         turno = 1;
+        System.out.println("¡Battle Space iniciado!");
+        System.out.println("Controles: A/D/W/S para moverse, E para disparar, Q para salir");
         mostrarEstado();
+        combate.mostrarCampo();
     }
 
-    
-    public void ejecutarTurno() {
+    public void ejecutarTurno(char tecla) {
         if (juegoTerminado) {
-            System.out.println("El juego ha terminado.");
             return;
         }
 
         System.out.println("Turno " + turno);
-
-    
-        jugador.atacar(enemigos.get(0));
-
-        
-        for (Enemigo enemigo : enemigos) {
-            if (enemigo.estaVivo()) {
-                enemigo.atacar(jugador);
-            }
-        }
-
+        combate.jugarJugador(tecla);
+        combate.jugarEnemigos();
+        combate.eliminarEnemigosDerrotados();
+        mostrarEstado();
+        combate.mostrarCampo();
         verificarFinDelJuego();
         turno++;
     }
 
-     
     public void verificarFinDelJuego() {
         if (!jugador.estaVivo()) {
             juegoTerminado = true;
             System.out.println("¡Has perdido! El jugador ha sido derrotado.");
-        }
-
-        if (enemigos.isEmpty()) {
+            System.out.println("Puntaje final: " + jugador.getPuntaje());
+        } else if (!hayEnemigosVivos()) {
             juegoTerminado = true;
             System.out.println("¡Has ganado! Todos los enemigos han sido derrotados.");
+            System.out.println("Puntaje final: " + jugador.getPuntaje());
         }
     }
-    
+
     public void mostrarEstado() {
-        System.out.println("Jugador: " + jugador.getNombre() + " - Salud: " + jugador.getSalud());
+        System.out.println("Jugador: " + jugador.getNombre() + " - Salud: " + jugador.getSalud() + 
+                           " - Ataque: " + jugador.getAtaque() + " - Puntaje: " + jugador.getPuntaje());
         System.out.println("Enemigos restantes:");
-        for (Enemigo enemigo : enemigos) {
-            enemigo.mostrarEstado();
+        for (Enemigo e : enemigos) {
+            if (e != null && e.estaVivo()) {
+                e.mostrarEstado();
+            }
         }
     }
 
     public void generarEnemigos(int cantidad) {
         for (int i = 0; i < cantidad; i++) {
-            int x = (int) (Math.random() * ANCHO);
-            int y = (int) (Math.random() * (ALTO / 2));
-            enemigos.add(new Enemigo("TipoA", 50, 10, x, y));
+            enemigos[i] = new Enemigo("NaveAlienigena", 50, 10, 2, 1 + i * 3); // Columnas 1, 4, 7, 10, 13
         }
     }
 
-    public void moverJugadorIzquierda() {
-        jugador.mover(-1, 0, ANCHO, ALTO);
-        renderizar();
-    }
-    
-    public void moverJugadorDerecha() {
-        jugador.mover(1, 0, ANCHO, ALTO);
-        renderizar();
-    }
-
-    public void moverJugadorArriba() {
-        jugador.mover(0, -1, ANCHO, ALTO);
-        renderizar();
-    }
-
-    public void moverJugadorAbajo() {
-        jugador.mover(0, 1, ANCHO, ALTO);
-        renderizar();
-    }
-    
-    public void dispararBala() {
-        jugador.disparar();
-    }
-
-    public void renderizar() {
-        for (int i = 0; i < ALTO; i++) {
-            for (int j = 0; j < ANCHO; j++) {
-                tablero[i][j] = ' ';
-            }
+    private boolean hayEnemigosVivos() {
+        for (Enemigo e : enemigos) {
+            if (e != null && e.estaVivo()) return true;
         }
+        return false;
+    }
 
-        tablero[jugador.obtenerY()][jugador.obtenerX()] = '^';  
-        for (Enemigo enemigo : enemigos) {
-            if (enemigo.estaVivo()) {
-                tablero[enemigo.obtenerY()][enemigo.obtenerX()] = '@'; 
-            }
-        }
-
-        for (int i = 0; i < ALTO; i++) {
-            for (int j = 0; j < ANCHO; j++) {
-                System.out.print(tablero[i][j]);
-            }
-            System.out.println();
-        }
+    public boolean isJuegoTerminado() {
+        return juegoTerminado;
     }
 }
