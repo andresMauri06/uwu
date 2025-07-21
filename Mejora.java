926a457dcb79de8cdb70405c974b52c32518b17f
andresMauri06 <andrejim609@gmail.com>
2025-07-20 22:54:28 -0500

    Add files via upload

---
+++ Mejora.java
@@ -0,0 +1,36 @@
+package battlespace;
+
+public class Mejora {
+    private String tipo;
+    private int valor;
+    private int fila;
+    private int columna;
+
+    public Mejora(String tipo, int valor, int fila, int columna) {
+        this.tipo = tipo;
+        this.valor = valor;
+        this.fila = fila;
+        this.columna = columna;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public int getValor() {
+        return valor;
+    }
+
+    public String descripcion() {
+        String tipoBonito = tipo.equals("vida") ? "Salud" : "Ataque";
+        return tipoBonito + " +" + valor;
+    }
+
+    public int getFila() {
+        return fila;
+    }
+
+    public int getColumna() {
+        return columna;
+    }
+}
